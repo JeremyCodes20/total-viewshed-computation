96a9dfb2b0b13e470c3be8fbe876ae5d2769b371
Jeremy Young <young.a.jeremy@gmail.com>
2021-12-02 14:28:57 -0700

    bresenham's line algorithm (basic) implemented

--- shared/main.cpp
+++ shared/main.cpp
@@ -7,6 +7,9 @@ const char* dem_location = "../data/srtm_14_04_6000x6000_short16.raw";
 
 int openDem(const char* location, short* dem);
 void testDemRead(short* dem);
+void bLineDown(int x0, int y0, int x1, int y1);
+void bLineUp(int x0, int y0, int x1, int y1);
+void bLine(int x0, int y0, int x1, int y1);
 
 int main()
 {
@@ -21,6 +24,8 @@ int main()
     }
     printf("Opened input file: %s\n", dem_location);
     // testDemRead(dem);
+
+    bLine(5, 5, 5, 0);
 
     delete[] dem;
     return 0;
@@ -50,11 +55,98 @@ int openDem(const char* location, short* dem)
     return 0;
 }
 
+/*
+    Read a few values from the dem array to verify that input read was successful
+*/
 void testDemRead(short* dem)
 {
     int upper_bound = map_width * map_width;
     for (int i = upper_bound - 100; i < upper_bound; ++i)
     {
         printf("%d\n", dem[i]);
+    }
+}
+
+/*
+    Calculate line when the slope is negative (or 0!!)
+    Source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+*/
+void bLineDown(int x0, int y0, int x1, int y1)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0)
+    {
+        yi = -1;
+        dy = -dy;
+    }
+    int D = (2 * dy) - dx;
+    int y = y0;
+
+    // NOTE: may be just <
+    for (int x = x0; x <= x1; ++x)
+    {
+        // add x,y to line collection
+        printf("(%d, %d)\n", x, y);
+        if (D > 0)
+        {
+            y += yi;
+            D += (2 * (dy - dx));
+        }
+        else
+        {
+            D += 2 * dy;
+        }
+    }
+}
+
+/*
+    Calculate line when the slope is positive
+    Source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+*/
+void bLineUp(int x0, int y0, int x1, int y1)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int xi = 1;
+    if (dx < 0)
+    {
+        xi = -1;
+        dx = -dx;
+    }
+    int D = (2 * dx) - dy;
+    int x = x0;
+
+    // NOTE: may be just <
+    for (int y = y0; y <= y1; ++y)
+    {
+        // add x,y to line collection
+        printf("(%d, %d)\n", x, y);
+        if (D > 0)
+        {
+            x += xi;
+            D += (2 * (dx - dy));
+        }
+        else
+        {
+            D += 2 * dx;
+        }
+    }
+}
+
+/*
+    Calculate what points are intersected by a line between (x0,y0) and (x1,y1)
+    Source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+*/
+void bLine(int x0, int y0, int x1, int y1)
+{
+    if (abs(y1 - y0) < abs(x1 - x0))
+    {
+        (x0 > x1) ? bLineDown(x1, y1, x0, y0) : bLineDown(x0, y0, x1, y1);
+    }
+    else
+    {
+        (y0 > y1) ? bLineUp(x1, y1, x0, y0) : bLineUp(x0, y0, x1, y1);
     }
 }
