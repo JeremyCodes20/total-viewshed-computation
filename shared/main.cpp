7d5f25ab14e0f7a1375bb29315b8be71591ca500
Jeremy Young <young.a.jeremy@gmail.com>
2021-12-02 13:48:27 -0700

    Input of DEM file complete

--- shared/main.cpp
+++ shared/main.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 
 const int map_width = 6000;
 const char* dem_location = "../data/srtm_14_04_6000x6000_short16.raw";
 
 int openDem(const char* location, short* dem);
+void testDemRead(short* dem);
 
 int main()
 {
     // Digital Elevation Map: 6000x6000 array of shorts
     short* dem = new short[map_width * map_width];
 
-    if (!openDem(dem_location, dem))
+    if (openDem(dem_location, dem))
     {
-        free(dem);
+        printf("Failed to open input file: %s\n", dem_location);
+        delete[] dem;
         return 1; // File failed to open, exit program
     }
+    printf("Opened input file: %s\n", dem_location);
+    // testDemRead(dem);
 
-    
-
-    free(dem);
+    delete[] dem;
     return 0;
 }
 
+/*
+    Open DEM from RAW file and store in 1-dimensional short array
+*/
 int openDem(const char* location, short* dem)
 {
     std::ifstream input_raw(location, std::ios::binary);
     if (!input_raw.is_open())
     {
-        std::cout << "Failed to open input file: ";
-        std::cout << location;
         return 1;
     }
 
@@ -44,4 +48,13 @@ int openDem(const char* location, short* dem)
 
     input_raw.close();
     return 0;
+}
+
+void testDemRead(short* dem)
+{
+    int upper_bound = map_width * map_width;
+    for (int i = upper_bound - 100; i < upper_bound; ++i)
+    {
+        printf("%d\n", dem[i]);
+    }
 }
